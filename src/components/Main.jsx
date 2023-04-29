f1beff4db7703817bfebc2722f56fb5ab1fad5d3
Jorge Luis <jorg3luislopezc1941@hotmail.com>
2023-04-29 18:57:19 -0500

    dos

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -36,11 +36,10 @@ const Main1 = () => {
             <div className='right'>
                 <div className='container__right'>
                     <div>
-                        <h2>Facíl y Accesible</h2>
-                        <p>Nuestra plataforma ha sido diseñada para lograr
-                        tener una gran gestion de clientes efectiva, que sea 
-                        facil de usar y accesible para cualquier persona, sin importar
-                        su nivel de experiencia tecnológica
+                        <h2>Personalizable y escalable</h2>
+                        <p>En nuestra plataforma, puedes personalizarla con tu marca y adaptar los servicios 
+                            según tus necesidades. Además, si tu negocio crece y necesitas más citas,
+                            nuestra plataforma puede crecer contigo.
                         </p>
                     </div>
                  </div>
@@ -55,11 +54,10 @@ const Main1 = () => {
             <div className='right'>
                 <div className='container__right'>
                     <div>
-                        <h2>Facíl y Accesible</h2>
-                        <p>Nuestra plataforma ha sido diseñada para lograr
-                        tener una gran gestion de clientes efectiva, que sea 
-                        facil de usar y accesible para cualquier persona, sin importar
-                        su nivel de experiencia tecnológica
+                        <h2>Segura y confiable</h2>
+                        <p>En nuestra plataforma nos tomaremos muy en serio la seguridad y la privacidad de la 
+                            información personal y financiera de tus clientes y sus clientes. Cumplimos con 
+                            los más altos estándares de seguridad y privacidad para asegurar que su información esté segura.
                         </p>
                     </div>
                  </div>
