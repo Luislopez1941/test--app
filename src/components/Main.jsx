fc8116a62163316b2a363d11ce65c3cf3b25ffd1
Jorge Luis <jorg3luislopezc1941@hotmail.com>
2023-04-29 18:29:54 -0500

    uno

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -10,12 +10,12 @@ const Main1 = () => {
       <div className='main__container'>
         <div className='main1'>
             <div className='left'>
-                <div>
+                <div className='container__left'>
                     <img src={img2} alt="" />
                 </div>
             </div>
             <div className='right'>
-                <div className='container__rigth'>
+                <div className='container__right'>
                     <div>
                         <h2>Facíl y Accesible</h2>
                         <p>Nuestra plataforma ha sido diseñada para lograr
@@ -27,33 +27,33 @@ const Main1 = () => {
                  </div>
             </div>
         </div>
-        <div className='main2'>
-        <div className='left2'>
-                <div className='container__left2'>
-                    <h2>Facíl y Accesible</h2>
-                    <p>Nuestra plataforma ha sido diseñada para lograr
-                    tener una gran gestion de clientes efectiva, que sea 
-                    facil de usar y accesible para cualquier persona, sin importar
-                    su nivel de experiencia tecnológica
-                    </p>
+        <div className='main1'>
+            <div className='left left__uno'>
+                <div className='container__left'>
+                    <img src={img3} alt="" />
                 </div>
             </div>
-            <div className='right2'>
-                <div className='container__rigth2'>
+            <div className='right'>
+                <div className='container__right'>
                     <div>
-                        <img className='img' src={img3} alt="" />
+                        <h2>Facíl y Accesible</h2>
+                        <p>Nuestra plataforma ha sido diseñada para lograr
+                        tener una gran gestion de clientes efectiva, que sea 
+                        facil de usar y accesible para cualquier persona, sin importar
+                        su nivel de experiencia tecnológica
+                        </p>
                     </div>
                  </div>
             </div>
         </div>
         <div className='main1'>
             <div className='left'>
-                <div>
+                <div className='container__left'>
                     <img src={img4} alt="" />
                 </div>
             </div>
             <div className='right'>
-                <div className='container__rigth'>
+                <div className='container__right'>
                     <div>
                         <h2>Facíl y Accesible</h2>
                         <p>Nuestra plataforma ha sido diseñada para lograr
